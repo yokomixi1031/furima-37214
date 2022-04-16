d7c5c0c09ea224b41f2a9cf8fa7b2f049633ec5a
yokomixi1031 <domybestflyhigh@gmail.com>
2022-04-16 22:04:06 +0900

    ユーザー管理機能の実装

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new 
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to root_path
+  end
+
+end
