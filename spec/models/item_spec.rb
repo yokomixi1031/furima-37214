baed4ee1385117ea1c8988e0d2630de0a0e9c693
yokomixi1031 <domybestflyhigh@gmail.com>
2022-04-23 19:36:26 +0900

    単体テストコード

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,63 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '新規登録' do
+    before do
+      @item = FactoryBot.build(:item)
+    end
+
+    context '内容に問題ない場合' do
+      it 'すべての情報があれば登録できる' do
+        expect(@item).to be_valid
+      end
+    end
+
+    context '内容に問題がある場合' do
+      it 'item_nameが空だと保存できないこと' do
+        @item.item_name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item name can't be blank")
+      end
+      it 'explanationが空では登録できない' do
+        @item.explanation = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Explanation can't be blank")
+      end
+      it '販売価格が300円より少ない場合は出品できない' do
+        @item.price = 299
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+      end
+      it '販売価格が9999999円より大きい場合は出品できない' do
+        @item.price = 100000000
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+      end
+      it '「カテゴリー」選択が「--」の時は保存できないようにする' do
+        @item.category_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+      it '「商品の状態」選択が「--」の時は保存できないようにする' do
+        @item.condition_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition can't be blank")
+      end
+      it '「配送料の負担」選択が「--」の時は保存できないようにする' do
+        @item.delivery_charge_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery charge can't be blank")
+      end
+      it '「配送元の地域」選択が「--」の時は保存できないようにする' do
+        @item.ship_from_area_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Ship from area can't be blank")
+      end
+      it '「発送までの日数」選択が「--」の時は保存できないようにする' do
+        @item.delivery_day_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery day can't be blank")
+      end
+    end
+  end
 end
