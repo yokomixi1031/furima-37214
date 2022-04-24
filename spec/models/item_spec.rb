2186e969cfa633978c42632daad6d135119414ef
yokomixi1031 <domybestflyhigh@gmail.com>
2022-04-24 14:35:15 +0900

    修正２

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -73,6 +73,11 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Delivery day can't be blank")
       end
+      it 'userが紐付いていないと保存できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include('User must exist')
+      end
     end
   end
 end
