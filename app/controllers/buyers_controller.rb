cda22769499df1c6bd5fe282c1bf8cf731a4910e
yokomixi1031 <domybestflyhigh@gmail.com>
2022-05-02 23:47:17 +0900

    sold out の表示分岐

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -1,5 +1,5 @@
 class BuyersController < ApplicationController
-  before_action :authenticate_user!, except: :index
+  before_action :authenticate_user!
 
   def index
     @item = Item.find(params[:item_id])
